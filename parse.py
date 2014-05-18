67c85e39dd89143204c93bd847c3c1423a1a643d
tj511 <tj511@doc.ic.ac.uk>
2014-05-18 16:45:26 +0100

    added comments to separate sections of amin script, added interactive input of parser parameters via stdin

--- parse.py
+++ parse.py
@@ -11,6 +11,9 @@ from version_parser import *
 logging.basicConfig(level=logging.DEBUG, format="%(name)-8s: %(levelname)-8s %(message)s")
 logger = logging.getLogger("parser")
 
+# --------------------------------- #
+#         Argument Parsing          #
+# --------------------------------- #
 
 parser = argparse.ArgumentParser(description='| Parse nvd xml files into sqlite db')
 parser.add_argument('filenames', metavar='nvd xml file', type=str, nargs='+',
@@ -22,28 +25,48 @@ parser.add_argument('--simulate', help='parse only, do not write to db', action=
 parser.add_argument('--emptydb', help='clear database before insertion', action='store_true', default=False)
 parser.add_argument('--database', help='database file to write to', type=str, default='data.sqlite')
 parser.add_argument('--salt', help='salt to encrypt version numbers with', type=str, default='salt')
+parser.add_argument('--interactive', help='enable interactive input of input parameters', action='store_true', default=False)
 
 args = parser.parse_args()
 
-hasher = Hashids(args.salt)
-
 filenames = args.filenames
+salt = args.salt
+emptydb = args.emptydb
+simulate = args.simulate
+database = args.database
+
+
+if args.interactive:
+  # Override arguments with any inputted into program
+  simulate = raw_input("Simulate? [Y/n]: ")
+  salt = raw_input("Salt? [salt]: ")
+  emptydb = raw_input("Empty DB? [y/N]: ")
+  database = raw_input("Output DB? [filename]: ")
+
+  simulate = simulate == 'y' or simulate == ''
+  salt = salt or args.salt
+  emptydb = emptydb == 'y'
+  database = database or args.database
+
+hasher = Hashids(salt)
+
+
+# --------------------------------- #
+#      Check XML files exist        #
+# --------------------------------- #
 
 for file in filenames:
-  # Check all inputted files exist
   if not os.path.exists(file):
     sys.exit("Error: %s does not exist" % file)
 
 logger.info("Reading in %s.." % (', '.join(filenames)))
 
-plugins = [
-  ("adobe", "flash_player"),
-  ("oracle", "jre"),
-  ("microsoft", "silverlight"),
-  ("apple", "quicktime")
-]
-
 vulnerabilities = []
+
+
+# -------------------------------------- #
+#   Read vulnerabilities from XML files  #
+# -------------------------------------- #
 
 for file in filenames:
   logger.info("Parsing %s.." % file)
@@ -52,8 +75,24 @@ for file in filenames:
   vs = p.get_vulnerabilities()
   vulnerabilities.extend(vs)
 
-# Open the database for insertion
-db = Database(args.database, empty=args.emptydb, simulate=args.simulate)
+
+# --------------------------------- #
+#       Open SQLite database        #
+# --------------------------------- #
+
+db = Database(database, empty=emptydb, simulate=simulate)
+
+
+# --------------------------------- #
+#   Plugins we are searching for    #
+# --------------------------------- #
+
+plugins = [
+  ("adobe", "flash_player"),
+  ("oracle", "jre"),
+  ("microsoft", "silverlight"),
+  ("apple", "quicktime")
+]
 
 # Insert the products we're searching for into the db to map to vulnerabilities
 for i in xrange(len(plugins)):
